e0431dabf2483929981c0523835e0f125108fb02
Felipe N Souza <fnscoder@gmail.com>
2021-06-29 00:49:31 -0300

    Created session model

--- events/models.py
+++ events/models.py
@@ -1,8 +1,15 @@
+import uuid
+
 from django.db import models
 
 
 class Event(models.Model):
-    session_id = models.UUIDField('session id')
+    session = models.ForeignKey(
+        'events.Session',
+        verbose_name='session',
+        related_name='events',
+        on_delete=models.CASCADE
+    )
     category = models.CharField('category', max_length=50)
     name = models.CharField('name', max_length=150)
     data = models.JSONField('payload data')
@@ -14,3 +21,8 @@ class Event(models.Model):
     class Meta:
         verbose_name = 'event'
         verbose_name_plural = 'events'
+
+
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField('created at', auto_now_add=True)
--- events/serializers.py
+++ events/serializers.py
@@ -1,9 +1,18 @@
+from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
 
-from events.models import Event
+from events.models import Event, Session
 
 
-class EventSerializer(serializers.ModelSerializer):
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = ('id',)
+
+
+class EventSerializer(WritableNestedModelSerializer):
+    session = SessionSerializer()
+
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ('id', 'session', 'category', 'name', 'data', 'timestamp')
