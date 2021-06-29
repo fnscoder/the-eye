62b6aa1f174193a4e9e4e2444eb1c991e8435100
Felipe N Souza <fnscoder@gmail.com>
2021-06-29 12:17:41 -0300

    Used select for update to avoid race conditions

--- events/views.py
+++ events/views.py
@@ -5,8 +5,12 @@ from events.serializers import ErrorSerializer, EventSerializer
 
 
 class EventModelViewSet(ModelViewSet):
-    queryset = Event.objects.all()
+    queryset = Event.objects.select_for_update().all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        if self.action in ('list', 'retrieve'):
+            return Event.objects.all()
 
 
 class ErrorModelViewSet(ReadOnlyModelViewSet):
